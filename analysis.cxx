19c575e0a562b6ad60dfc5bea3e0e56b07ad1fef
William Cohen <wcohen@redhat.com>
2021-09-21 15:36:08 -0400

    Add support to handle 32-bit x86 binaries
    Dyninst liveness analysis treats the analysis of 32-bit x86 and 64-bit
    x86_64 code differently.  In dyninst the register names are different.
    Thus, trying to get the liveness results for rdi (the 64-bit register
    name) on 32-bit analysis will fail.  The analysis also needs to treat
    32-bit and 64-bit a bit differently as there are difference in the
    calling ABIs.
    
    Right now the code dummy up using edi/rdi register which is the holds
    the first argument in the register.  Doing this because the
    location_context cts doesn't hold information outside the variable name.

--- analysis.cxx
+++ analysis.cxx
@@ -46,7 +46,7 @@ analysis::analysis(char *name)
 }
 
 // FIXME: Currently only support x86_64 will need to set up for powerpc and aarch64
-static const MachRegister dyninst_register[] = {
+static const MachRegister dyninst_register_64[] = {
 	x86_64::rax,
 	x86_64::rdx,
 	x86_64::rcx,
@@ -66,6 +66,16 @@ static const MachRegister dyninst_register[] = {
 	x86_64::rip
 };
 
+static const MachRegister dyninst_register_32[] = {
+	x86::eax,
+	x86::edx,
+	x86::ecx,
+	x86::ebx,
+	x86::esi,
+	x86::edi,
+	x86::ebp,
+	x86::esp,
+};
 
 int liveness(const char *executable,
 	     Dwarf_Addr addr,
@@ -74,6 +84,10 @@ int liveness(const char *executable,
 	// should cache the executable names like the other things
 	char *exe = strdup(executable);
 	analysis func_to_analyze(exe);
+	MachRegister r;
+
+	// Determine whether 32-bit or 64-bit code as the register names are different in dyninst
+	int reg_width = func_to_analyze.co->cs()->getAddressWidth();
 
 	#if 0
 	// Find where the variable is located
@@ -82,48 +96,51 @@ int liveness(const char *executable,
 	// If variable isn't in a register, punt (return 0)
 	if (loc->type != loc_register) return 0;
 
-	// Map dwarf number to dyninst number, skip if out of range
-	if (loc->offset >= sizeof(dyninst_register)/sizeof(MachRegister)) return 0;
-	MachRegister r = dyninst_register[loc->offset];
+	// Map dwarf number to dyninst register name, punt if out of range
+	unsigned int regno = loc->offset;
+	#else
+	unsigned int regno = 5; // Dummy up to arg1 of x86
 	#endif
+	switch (reg_width){
+	case 4:
+		if (regno >= (sizeof(dyninst_register_32)/sizeof(MachRegister))) return 0;
+		r = dyninst_register_32[regno]; break;
+	case 8:
+		if (regno >= (sizeof(dyninst_register_64)/sizeof(MachRegister))) return 0;
+		r = dyninst_register_64[regno]; break;
+	default:
+		cout << "Reg width " << reg_width << " bytes not understood." << endl;
+		return 0;
+	}
 
-	// Find the function containing address
-	cout << "liveness analysis " << executable << " " << hex << addr << endl;
+	// Find the function containing the probe point.
 	vector<CodeRegion *> rr = func_to_analyze.co->cs()->regions();
-	std::set<ParseAPI::Function*> ff;
-	if(func_to_analyze.co->findFuncs(rr[0], addr, ff) <= 0) return 0;
-	ParseAPI::Function *func = *ff.begin();
-	cout << "<" << func->addr() << ">:" << func->name() << "[" << addr << "]" << endl;
-	// Check to see if a previous liveness information exists for function to reuse
+	std::set<ParseAPI::Function*> ff_s;
+	if(func_to_analyze.co->findFuncs(rr[0], addr, ff_s) <= 0) return 0;
+	ParseAPI::Function *func = *ff_s.begin();
+
+	// FIXME Check to see if a previous liveness information exists for function to reuse
 	// Otherwise create new liveness analysis
-	LivenessAnalyzer la(func->obj()->cs()->getAddressWidth());
+	LivenessAnalyzer la(reg_width);
 	la.analyze(func);
-	// Construct a liveness query location for the function entry.
-	// Get basic block containing the instruction
+
+	// Get the basic block and instruction containing the the probe point.
 	set<Block *> bb_s;
 	if (func_to_analyze.co->findBlocks(rr[0], addr, bb_s) != 1 )
-		return 0; // problem find basic block, punt
+		return 0; // too many (or too few) basic blocks, punt
 	Block *bb = *bb_s.begin();
 	Instruction curInsn = bb->getInsn(addr);
 
-	// Construct a liveness query location for the function entry.
+	// Construct a liveness query location for the probe point.
 	InsnLoc i(bb,  addr, curInsn);
 	Location iloc(func, i);
 
-	#if 0
 	// Query to see if whether the register is live at that point
 	bool used;
 	la.query(iloc, LivenessAnalyzer::Before, r, used);
-	cout << r <<  (used ? " used"  : "unused") << endl;
+	cout << "liveness analysis " << executable << " " << hex << addr << endl;
+	cout << r.name() <<  (used ? " used"  : " unused") << endl;
 	return (used ? 1 : -1);
-	#else
-	// Query to list out the registers that are live at that point
-	// FIXME: This is because not currently getting the actual register
-	bitArray liveRegs;
-	la.query(iloc, LivenessAnalyzer::Before, liveRegs);
-	cout << "liveRegs " << liveRegs << endl;
-	return 0;
-	#endif
 }
 
 #endif // HAVE_DYNINST
