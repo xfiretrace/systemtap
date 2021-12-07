209b5a19c6ee30c2db3f6c849620544a851013e8
Stan Cox <scox@redhat.com>
2021-12-07 09:55:01 -0500

    sys/sdt.h fp constraints: aarch64, s390
    Remove float constraints as per commit 1d3653936 but for aarch64 and s390.

--- includes/sys/sdt.h
+++ includes/sys/sdt.h
@@ -102,10 +102,6 @@
 # define STAP_SDT_ARG_CONSTRAINT        nZr
 # elif defined __x86_64__
 # define STAP_SDT_ARG_CONSTRAINT norx
-# elif defined __aarch64__
-# define STAP_SDT_ARG_CONSTRAINT norw
-# elif defined __s390__ || defined __s390x__
-# define STAP_SDT_ARG_CONSTRAINT        norf
 # elif defined __arm__
 # define STAP_SDT_ARG_CONSTRAINT        g
 # else
