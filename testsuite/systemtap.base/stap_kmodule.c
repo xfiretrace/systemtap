2efbb73243bf733abefa2f9f7b77fa0dc536f383
Stan Cox <scox@redhat.com>
2022-03-03 15:40:42 -0500

    Use timer instead of procfs to interface kmodule test with stap probe.
    Replace use of procfs with timer in stap_kmodule.c, add return probe to
    kmodule.stp and kprobe_module.stp, remove procfs bits from kmodule.exp

--- testsuite/systemtap.base/stap_kmodule.c
+++ testsuite/systemtap.base/stap_kmodule.c
@@ -9,11 +9,10 @@
  */
 
 #include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/proc_fs.h>
-#include <linux/compiler.h>
-#include <linux/sched.h>
-#include <linux/uaccess.h>
+#include <linux/timer.h>
+#include <linux/jiffies.h>
 
 /*
  * The purpose of this module is to provide a function that can be
@@ -25,53 +24,31 @@
 
 /************ Below are the functions to create this module ************/
 
-static struct proc_dir_entry *stm_ctl = NULL;
+struct timer_list simple_timer;
+static const int timer_interval = 5;
 
-static int last_cmd = -1;
-
-static ssize_t stm_write_cmd (struct file *file, const char __user *buf,
-			       size_t count, loff_t *ppos)
+static void simple_timer_function(struct timer_list *timer)
 {
-	char type;
-
-	if (get_user(type, (char __user *)buf))
-		return -EFAULT;
-
-	switch (type) {
-	case '0':
-		last_cmd = 0;
-		break;
-	case '1':
-		last_cmd = 1;
-		break;
-	case '2':
-		last_cmd = 2;
-		break;
-	default:
-		printk ("stap_kmodule: invalid command type %d\n", (int)type);
-		return -EINVAL;
+	static int count;
+	mod_timer (&simple_timer, jiffies + ( msecs_to_jiffies(timer_interval)));
+	if (count) {
+	  count = 0;
 	}
-  
-	return count;
+	else {
+	  count = 1;
+	}
 }
-
-static struct file_operations stm_fops_cmd = {
-	.owner = THIS_MODULE,
-	.write = stm_write_cmd,
-};
 
 int init_module(void)
 {
-	stm_ctl = proc_create ("stap_kmodule_cmd", 0666, NULL, &stm_fops_cmd);
-	if (stm_ctl == NULL) 
-		return -1;
+	timer_setup (&simple_timer, simple_timer_function,0);
+	mod_timer (&simple_timer, jiffies + msecs_to_jiffies(timer_interval));
 	return 0;
 }
 
 void cleanup_module(void)
 {
-	if (stm_ctl)
-		remove_proc_entry ("stap_kmodule_cmd", NULL);
+	del_timer (&simple_timer);
 }
 
 MODULE_DESCRIPTION("systemtap test module");
