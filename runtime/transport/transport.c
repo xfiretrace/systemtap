18031a4ef6279180725e65319cd46f8a52efc6b6
Frank Ch. Eigler <fche@redhat.com>
2022-04-25 11:51:49 -0400

    transport: fix subbuf size to PAGE_SIZE
    High-message-rate stap scripts more easily lose message synch or bog
    down if the subbuf size is large.  PAGE_SIZE appears to be a sweet
    spot, so let's fix that.  (At least one subbuf is used per probe hit
    that produces output.  Allocation occurs at the subbuf granularity, so
    making it smaller is apparently of no advantage.)  stap -s and
    free-memory still affect transport memory allocation, but only as to
    the number of subbufs.

--- runtime/transport/transport.c
+++ runtime/transport/transport.c
@@ -75,8 +75,8 @@ static inline void _stp_unlock_inode(struct inode *inode);
 /* set default buffer parameters.  User may override these via stap -s #, and
    the runtime may auto-shrink it on low memory machines too. */
 /* NB: Note default in man/stap.1.in */
-static unsigned _stp_nsubbufs = 128;
-static unsigned _stp_subbuf_size = 2 * STP_BUFFER_SIZE; /* 2 * 64K */
+static unsigned _stp_nsubbufs = 16*1024*1024 / PAGE_SIZE;
+static unsigned _stp_subbuf_size = PAGE_SIZE;
 
 /* module parameters */
 static int _stp_bufsize;
@@ -610,8 +610,8 @@ static int _stp_transport_init(void)
                 long _stp_bufsize_avail;
                 si_meminfo(&si);
                 _stp_bufsize_avail = (long)((si.freeram + si.bufferram) / 4 / num_online_cpus())
-                        << PAGE_SHIFT; // limit to quarter of free ram total
-                if ((_stp_nsubbufs * _stp_subbuf_size * num_online_cpus()) > _stp_bufsize_avail) {
+                        << PAGE_SHIFT; // limit to quarter of free ram total, divided between cpus
+                if ((_stp_nsubbufs * _stp_subbuf_size) > _stp_bufsize_avail) {
                         _stp_bufsize = max_t (int, 1, _stp_bufsize_avail / 1024 / 1024);
                         dbug_trans(1, "Shrinking default _stp_bufsize to %d MB/cpu due to low free memory\n", _stp_bufsize);
                 }
@@ -619,15 +619,12 @@ static int _stp_transport_init(void)
         
 	if (_stp_bufsize) { // overridden by user or by si_meminfo heuristic?
 		long size = _stp_bufsize * 1024 * 1024;
-		_stp_subbuf_size = 65536;
-                // bump up subbuf size from 64K to 1M to keep _stp_nsubbufs not too large
-		while (size / _stp_subbuf_size > 64 &&
-		       _stp_subbuf_size < 1024 * 1024) {
-			_stp_subbuf_size <<= 1;
-		}
+		_stp_subbuf_size = 1 << PAGE_SHIFT; // XXX: allow this to be bumped up too?
 		_stp_nsubbufs = size / _stp_subbuf_size;
 	}
-        dbug_trans(1, "Using %d subbufs of size %d\n", _stp_nsubbufs, _stp_subbuf_size);
+
+        dbug_trans(1, "Using %d subbufs of size %d * %d CPUs\n",
+                   _stp_nsubbufs, _stp_subbuf_size, num_online_cpus());
         
 	ret = _stp_transport_fs_init(THIS_MODULE->name);
 	if (ret)
