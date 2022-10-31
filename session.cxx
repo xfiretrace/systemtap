9b7ba04bb6c9fabee98749ee3e608225db785099
Serhei Makarov <serhei@serhei.io>
2022-10-31 12:46:31 -0400

    session.cxx: tested kernel range

--- session.cxx
+++ session.cxx
@@ -499,7 +499,7 @@ systemtap_session::version ()
              "Copyright (C) 2005-2022 Red Hat, Inc. and others\n"   // PRERELEASE
              "This is free software; see the source for copying conditions.\n",
              version_string().c_str());
-  cout << _F("tested kernel versions: %s ... %s\n", "2.6.32", "5.18-rc3");   // PRERELEASE
+  cout << _F("tested kernel versions: %s ... %s\n", "2.6.32", "6.0.0-rc5");   // PRERELEASE
   
   cout << _("enabled features:")
 #ifdef HAVE_AVAHI
